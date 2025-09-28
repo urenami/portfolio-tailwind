98db180e1c9461e0e8f7930870357d1f65101f00
urenami <122377715+urenami@users.noreply.github.com>
2025-09-28 13:47:50 -0400

    initial commit - tailwind working

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gradient-to-r from-purple-600 to-blue-500">
+      <h1 className="text-6xl font-extrabold text-white drop-shadow-lg">
+        Tailwind v4 Gradient Fixed ✅
+      </h1>
+    </div>
+  );
+}
+
+export default App;
