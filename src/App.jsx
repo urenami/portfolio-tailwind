498cfbe1226d4501896608f31344e0fa8a5473fe
urenami <122377715+urenami@users.noreply.github.com>
2025-09-30 10:38:25 -0400

    fixed Projects.jsx import casing error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
-import Projects from "./components/projects";
+import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
 function App() {
